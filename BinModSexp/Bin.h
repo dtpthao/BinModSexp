0c00fe019823770b40071f4f14cbf4893542e0cf
dtpthao <dtpthao.its@gmail.com>
2019-12-02 14:42:12 +0200

    add new binn using global vars and compare

--- BinModSexp/Bin.h
+++ BinModSexp/Bin.h
@@ -27,6 +27,7 @@ void compare_bin3s(big P, csprng &Rng, Result &res);
 /* pownbin.cpp */
 void powmodn_Bin(int n, big *r, big *y, big P, big &R);
 void testnBin(big P, csprng &Rng);
+void compare_binns(big P, csprng &Rng, Result &res);
 
 #endif // !_BIN_H
 
--- BinModSexp/pownbin.cpp
+++ BinModSexp/pownbin.cpp
@@ -62,7 +62,66 @@ void powmodn_Bin(int n, big *r, big *y, big P, big &R)
 	for (i = 0; i < tmp; i++) mirkill(pre_lst[i]);
 }
 
-#define TESTn 1
+// This is complicated to explain in condensed words
+// for example with n = 3:
+//  0   1   2    3     4    5      6        7
+// [1, y1, y2, y1*y2, y3, y1*y3, y2*y3, y1*y2*y3]
+void prepowmodn_Bin_gl(int n, big *y, big *pre_list, big P)
+{
+	int idx;
+	for (int i = 0; i < n; i++) {
+		idx = 1 << i;
+		copy(y[i], pre_list[idx]);
+		//printf("pre_list[%d]: ", idx); cotnum(pre_list[idx], stdout);
+		for (int j = 1; j < idx; j++) {
+			mulmod(pre_list[j], pre_list[idx], P, pre_list[idx + j]);
+			//printf("pre_list[%d]: ", idx + j); cotnum(pre_list[idx + j], stdout);
+		}
+	}
+}
+
+// R = y1^r1 * y2^r2 * ... * yn^rn mod P;
+void powmodn_Bin_gl(int n, big *r, big *y, big P, big &R)
+{
+	int i, j = 0, tmp;
+	DWORD last32,
+		*w = new DWORD[n];
+	prepowmodn_Bin(n, y, gl_bigs, P);
+
+	i = r[n - 1]->len - 1;
+	last32 = r[n - 1]->w[i];
+	while (last32 >> j && j != 32) j++;
+
+	R->len = 1; R->w[0] = 1;
+	for (--j; i >= 0; i--, j = 31) {
+		tmp = 0;
+		for (int ii = 0; ii < n; ii++) w[ii] = r[ii]->w[i];
+		while (j) {
+			for (int ii = 0; ii < n; ii++) {
+				tmp += ((w[ii] >> j) & 1) << ii;
+			}
+			//printf("index: %d\n", tmp);
+			mulmod(R, R, P, R);
+			//cout << "R^2: "; cotnum(R, stdout);
+			if (tmp) mulmod(R, gl_bigs[tmp], P, R);
+			//cout << "R: "; cotnum(R, stdout);
+			j--;
+			tmp = 0;
+		}
+		for (int ii = 0; ii < n; ii++) {
+			tmp += (w[ii] & 1) << ii;
+		}
+		//printf("tmp: %d\n", tmp);
+		mulmod(R, R, P, R);
+		//cout << "R^2: "; cotnum(R, stdout);
+		if (tmp) mulmod(R, gl_bigs[tmp], P, R);
+		//cout << "R: "; cotnum(R, stdout);
+		//cout << endl;
+	}
+	tmp = 1 << n;
+}
+
+#define TESTn 5000
 #define N 5
 void testnBin(big P, csprng &Rng)
 {
@@ -108,4 +167,78 @@ void testnBin(big P, csprng &Rng)
 		mirkill(y[i]);
 	}
 	mirkill(R); mirkill(R1);mirkill(R2); mirkill(k);
+}
+
+#define REPEATn 10
+void compare_binns(big P, csprng &Rng, Result &res)
+{
+	stopWatch timer1, timer2, timer3, timer4;
+	LONGLONG dur1, min1 = LONG_MAX,
+		dur2, min2 = LONG_MAX,
+		dur3, min3 = LONG_MAX,
+		dur4, min4 = LONG_MAX;
+
+	big	g = mirvar(1), k = mirvar(1),
+		R = mirvar(1), R1 = mirvar(1),
+		R2 = mirvar(1), R3 = mirvar(1);
+	big y[N], r[N];
+	for (int i = 0; i < N; i++) {
+		y[i] = mirvar(1);
+		r[i] = mirvar(1);
+	}
+
+	strong_bigrand(&Rng, P, g);
+
+	int count1 = 0, count2 = 0;
+	for (int i = 0; i < TESTn; i++) {
+		strong_bigrand(&Rng, P, k);
+		ShamirDecomposit_n(3, k, g, r, y, P);
+
+		for (int i = 0; i < REPEATn; i++) {
+			startTimer(&timer1);
+			powmod3_Bin(r, y, P, R1);
+			stopTimer(&timer1);
+			dur1 = getTickCount(&timer1);
+			min1 = (min1 < dur1) ? min1 : dur1;
+
+			startTimer(&timer2);
+			powmodn_Bin_gl(3, r, y, P, R2);
+			stopTimer(&timer2);
+			dur2 = getTickCount(&timer2);
+			min2 = (min2 < dur2) ? min2 : dur2;
+
+			startTimer(&timer3);
+			powmodn_Bin(3, r, y, P, R3);
+			stopTimer(&timer3);
+			dur3 = getTickCount(&timer3);
+			min3 = (min3 < dur3) ? min3 : dur3;
+
+			startTimer(&timer4);
+			powmodn(3, y, r, P, R);
+			stopTimer(&timer4);
+			dur4 = getTickCount(&timer4);
+			min4 = (min4 < dur4) ? min4 : dur4;
+		}
+		res.c[0] += !compare(R, R1);
+		res.c[1] += !compare(R, R2);
+		res.c[2] += !compare(R, R3);
+		res.t[0] += min1;
+		res.t[1] += min2;
+		res.t[2] += min3;
+		res.t[3] += min4;
+	}
+	res.t[0] /= TESTn;
+	res.t[1] /= TESTn;
+	res.t[2] /= TESTn;
+	res.t[3] /= TESTn;
+	res.p[1] = (res.t[1] / res.t[0]) * 100;
+	res.p[2] = (res.t[2] / res.t[0]) * 100;
+	res.p[3] = (res.t[3] / res.t[0]) * 100;
+
+	for (int i = 0; i < N; i++) {
+		mirkill(y[i]);
+		mirkill(r[i]);
+	}
+	mirkill(g); mirkill(k);
+	mirkill(R1); mirkill(R2); mirkill(R3); mirkill(R);
 }
